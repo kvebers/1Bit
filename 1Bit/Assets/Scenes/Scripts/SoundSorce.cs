b9fbd947a9f7eedeaf9a35b6fa061270ac5a1052
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-08-14 15:54:12 +0200

    Fix

--- 1Bit/Assets/Scenes/Scripts/SoundSorce.cs
+++ 1Bit/Assets/Scenes/Scripts/SoundSorce.cs
@@ -7,7 +7,7 @@ public class SoundSorce : MonoBehaviour
 
     void SpawnAndInitializePrefab()
     {
-        Vector3 newPosition = new Vector3(transform.position.x, 10, transform.position.z);
+        Vector3 newPosition = new Vector3(transform.position.x, 5, transform.position.z);
         GameObject newEcho = Instantiate(echoPrefab, newPosition, Quaternion.Euler(90f, 0f, 0f));
         Echo echoScript = newEcho.GetComponent<Echo>();
 
