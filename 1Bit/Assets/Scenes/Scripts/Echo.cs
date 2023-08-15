7051951dea773bbcfdc9460bf8dccf09efab654f
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-08-15 13:55:58 +0200

    Added an Echo Effect for the player. Should now create consistant reasons when it  could be called, for example steps, actions and so on

--- 1Bit/Assets/Scenes/Scripts/Echo.cs
+++ 1Bit/Assets/Scenes/Scripts/Echo.cs
@@ -1,3 +1,4 @@
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class Echo : MonoBehaviour
@@ -6,7 +7,32 @@ public class Echo : MonoBehaviour
 
     public void Initialize(float strength)
     {
-            _strength = strength;
+            _strength = strength * 120;
     }
 
+    private void Update()
+    {
+        if (_strength >= 60)
+        {
+            float scaleFactor = 1.004f;
+            
+            transform.localScale = new Vector3(transform.localScale.x * scaleFactor,
+                                               transform.localScale.y * scaleFactor,
+                                               transform.localScale.z * scaleFactor);
+            
+        }
+        else if (_strength >= 0)
+        {
+            float scaleFactor = 1.003f;
+            transform.localScale = new Vector3(transform.localScale.x * scaleFactor,
+                                               transform.localScale.y * scaleFactor,
+                                               transform.localScale.z * scaleFactor);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        _strength--;
+
+    }
 }
