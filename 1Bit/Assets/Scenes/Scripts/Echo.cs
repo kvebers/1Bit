60f09c60169fc335b283de8dde2a5c698415f5e8
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-08-15 18:02:39 +0200

    Small Uodate

--- 1Bit/Assets/Scenes/Scripts/Echo.cs
+++ 1Bit/Assets/Scenes/Scripts/Echo.cs
@@ -1,5 +1,10 @@
+using System;
 using TMPro.EditorUtilities;
+using Unity.VisualScripting;
+using UnityEditor.Rendering.PostProcessing;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+using UnityEngine.Timeline;
 
 public class Echo : MonoBehaviour
 {
@@ -7,32 +12,28 @@ public class Echo : MonoBehaviour
 
     public void Initialize(float strength)
     {
-            _strength = strength * 120;
+        _strength = strength * 60;
     }
 
     private void Update()
     {
-        if (_strength >= 60)
+        if (_strength > 0)
         {
-            float scaleFactor = 1.004f;
-            
-            transform.localScale = new Vector3(transform.localScale.x * scaleFactor,
-                                               transform.localScale.y * scaleFactor,
-                                               transform.localScale.z * scaleFactor);
-            
-        }
-        else if (_strength >= 0)
-        {
-            float scaleFactor = 1.003f;
-            transform.localScale = new Vector3(transform.localScale.x * scaleFactor,
-                                               transform.localScale.y * scaleFactor,
-                                               transform.localScale.z * scaleFactor);
+            float scaleFactor = 5; // Adjust this value as needed
+
+            transform.localScale = new Vector3(transform.localScale.x + scaleFactor,
+                                               transform.localScale.y + scaleFactor,
+                                               transform.localScale.z + scaleFactor);
+
+            _strength--;
         }
         else
         {
             Destroy(gameObject);
         }
-        _strength--;
-
     }
 }
+
+
+
+
