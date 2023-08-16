60b26bb2cefa21b58e95da883215a058c8f9125f
Niko114 <nikob114@gmail.com>
2023-08-16 15:18:49 +0200

    Added levels and raycast
    Added new tutorial & lvl1 and added looking at collider raycast

--- 1Bit/Assets/Scenes/Scripts/CharacterMovement.cs
+++ 1Bit/Assets/Scenes/Scripts/CharacterMovement.cs
@@ -4,38 +4,32 @@ using UnityEngine;
 
 public class CharacterControllerScript : MonoBehaviour
 {
-	public float walkSpeed;
-	public float gravity;
-	public float velocityY;
+	public float	walkSpeed;
+	public float	gravity;
+	public float	velocityY;
+	public float	mouseSensitivity = 2f;
+	public float	InteractionRange;
 
-	CharacterController controller;
+	public bool	lockCamera = false;
 
-	public Transform playerCamera = null;
-	public float mouseSensitivity = 2f;
+	public Vector2	mouseDelta = new Vector2(0,0);
 
-	private bool lockCursor = true;
+	public string	inp_moX = "Mouse X";
+	public string	inp_moY = "Mouse Y";
+	public string	inp_mvX = "Horizontal";
+	public string	inp_mvY = "Vertical";
 
-	public bool lockCamera = false;
+	public Transform	playerCamera;
+	public SoundSorce	soundSource;
 
-	float cameraPitch = 0.0f;
-	public Vector2 mouseDelta = new Vector2(0,0);
-
-	public string inp_moX = "Mouse X";
-	public string inp_moY = "Mouse Y";
-	public string inp_mvX = "Horizontal";
-	public string inp_mvY = "Vertical";
-
-	public SoundSorce soundSource;
+	CharacterController	controller;
+	float	cameraPitch = 0.0f;
+	private bool	lockCursor = true;
+	private GameObject	lookingAt;
 
 
 	private void Start()
 	{
-		if (lockCursor)
-		{
-			Cursor.lockState = CursorLockMode.Locked;
-			Cursor.visible = false;
-		}
-		controller = GetComponent<CharacterController>();
 		try
 		{
 			Input.GetAxis(inp_moX);
@@ -47,7 +41,20 @@ public class CharacterControllerScript : MonoBehaviour
 		{
 			print("Something in the InputManager is not working!!! Character Movement disabled.");
 			enabled = false;
+			return ;
 		}
+		if (lockCursor)
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+		if (!playerCamera)
+		{
+			print("No camura.\nbaka");
+			enabled = false;
+			return ;
+		}
+		controller = GetComponent<CharacterController>();
 		print("Press 'Space' for a white cylinder.\nPress 'L' to un-/lock mouse movement.\n" +
 			"Press 'esc' to make mouse visible.");
 	}
@@ -57,7 +64,7 @@ public class CharacterControllerScript : MonoBehaviour
 		if (!lockCamera)
 			UpdateMouseLook();
 		UpdateMovement();
-
+		CheckInteractables();
 	}
 
 	void UpdateMouseLook()
@@ -66,7 +73,7 @@ public class CharacterControllerScript : MonoBehaviour
 		cameraPitch = Mathf.Clamp(cameraPitch - ( mouseDelta.y * mouseSensitivity ), -90.0f, 90.0f);
 
 		playerCamera.localEulerAngles = Vector3.right * cameraPitch;
-		transform.Rotate(Vector3.up * mouseDelta.x * mouseSensitivity);
+		transform.Rotate(mouseDelta.x * mouseSensitivity * Vector3.up);
 	}
 
 	void UpdateMovement()
@@ -81,10 +88,40 @@ public class CharacterControllerScript : MonoBehaviour
 		controller.Move(velocity * Time.deltaTime);
 	}
 
-	private void OnControllerColliderHit(ControllerColliderHit hit)
+	void CheckInteractables()
 	{
-		//Do something, as soon Collider hits something
-		//(It will also hit the floor while normally walking)
+		RaycastHit[] hit = Physics.RaycastAll(playerCamera.transform.position, playerCamera.transform.forward, InteractionRange);
+		int important = -1;
+		for (int i = 0; i < hit.Length; i++)
+		{
+			if (!hit[i].transform.gameObject.CompareTag("Player"))
+			{
+				if (important == -1 || hit[important].distance > hit[i].distance)
+					important = i;
+			}
+		}
+
+		GameObject gogo = (important >= 0) ? hit[important].transform.gameObject : null;
+		if (lookingAt && lookingAt != gogo)
+		{
+			print($"Player STOPPED looking at collider named <{lookingAt.name}>");
+			lookingAt = null;
+		}
+		if (gogo)
+		{
+			if (!lookingAt)
+			{
+				lookingAt = gogo;
+				print($"Player STARTED now looking at collider named <{lookingAt.name}>");
+			}
+			print($"Player is CURRENTLY looking at collider named <{lookingAt.name}>");
+		}
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(playerCamera.transform.position, playerCamera.transform.position + playerCamera.transform.forward * InteractionRange);
 	}
 
 	public void OnGUI()
@@ -93,7 +130,7 @@ public class CharacterControllerScript : MonoBehaviour
 		if (e.isKey && e.type == EventType.KeyDown)
 		{
 			if (e.keyCode == KeyCode.L)
-				lockCamera = !( lockCamera );
+				lockCamera = !(lockCamera);
 			else if (e.keyCode == KeyCode.Space && soundSource != null)
 				soundSource.SpawnButtonClicked();
 		}
