a4e9fa6877ce5d3ab99a31932ce4dbebbd8eeaa4
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-08-18 11:31:58 +0200

    Added Intro Text

---
+++ 1Bit/Assets/TutorialText.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class TutorialText : MonoBehaviour
+{
+    public TextMeshProUGUI Text;
+    void Start()
+    {
+        Text.text = "Tutorial";
+        StartCoroutine(ChangeTextAfterDelay(2f, "Use WASD, and mose to move around"));
+        StartCoroutine(ChangeTextAfterDelay(4f, "Use ECHO location to find EXIT"));
+        StartCoroutine(ChangeTextAfterDelay(6f, ""));
+        DestroyTextAfterDelay(8f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator ChangeTextAfterDelay(float delay, string txt)
+    {
+        yield return new WaitForSeconds(delay);
+        Text.text = txt;
+    }
+
+    IEnumerator DestroyTextAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(Text.gameObject);
+    }
+}
